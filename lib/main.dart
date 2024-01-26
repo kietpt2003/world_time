bb434657e01976e6631272df213c36fc917a55f1
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-27 00:49:56 +0700

    config firebase to main and comment world time to learn firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,66 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:world_time/features/app/splash_screen/splash_screen.dart';
+import 'package:world_time/features/user_auth/presentation/pages/login_page.dart';
 import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation(),
-    },
-  ));
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDp0Ow8CzrWTMy0Dndd8Lng_6dNs78aYSA",
+      projectId: "flutterfirebase-faed4",
+      messagingSenderId: "1032027946166",
+      appId: "1:1032027946166:android:dd091144615b671471fa17",
+    ),
+  );
+  // final providers = [EmailAuthProvider()];
+  // runApp(MaterialApp(
+  //   initialRoute: '/',
+  //   routes: {
+  //     '/': (context) => Loading(),
+  //     '/home': (context) => Home(),
+  //     '/location': (context) => ChooseLocation(),
+  //     '/sign-in': (context) {
+  //       return SignInScreen(
+  //         providers: providers,
+  //         actions: [
+  //           AuthStateChangeAction<SignedIn>((context, state) {
+  //             Navigator.pushReplacementNamed(context, '/profile');
+  //           }),
+  //         ],
+  //       );
+  //     },
+  //     '/profile': (context) {
+  //       return ProfileScreen(
+  //         providers: providers,
+  //         actions: [
+  //           SignedOutAction((context) {
+  //             Navigator.pushReplacementNamed(context, '/sign-in');
+  //           }),
+  //         ],
+  //       );
+  //     },
+  //   },
+  // ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      home: SplashScreen(
+        child: LoginPage(),
+      ),
+    );
+  }
 }
