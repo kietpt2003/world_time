e8e7e58f5e6c2936edacc525bbd6be5d8903044e
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-29 00:45:16 +0700

    fixing homepage for using toast and comment unused back arrow

--- lib/features/user_auth/presentation/pages/home_page.dart
+++ lib/features/user_auth/presentation/pages/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:world_time/global/common/toast.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,9 +16,51 @@ class HomePage extends StatelessWidget {
           ),
         ),
         backgroundColor: Colors.blue,
+        // leading: const BackButton(
+        //   color: Colors.white,
+        // ),
       ),
-      body: const Center(
-        child: Text("Welcome to home page"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              "Welcome to home page",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.pushReplacementNamed(context, "/login");
+              showToast(message: 'Successfully Signed out');
+            },
+            child: Container(
+              height: 45,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  "Sign out",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
